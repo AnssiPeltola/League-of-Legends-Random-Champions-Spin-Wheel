36cb8d18bdc7aaacec6f15a45333b4d0a84a400d
Anssi Peltola <anssipeltola@hotmail.com>
2023-09-14 20:20:28 +0300

    I made many changes
    Now it checks all champions from LoL Champion API and the lenght of it for segments

--- scripts.js
+++ scripts.js
@@ -1,41 +1,66 @@
-let theWheel = new Winwheel({
-  canvasId: "canvas",
-  numSegments: 5,
-  fillStyle: "#5D00D3",
-  lineWidth: 0.3,
-  textAlignment: "center", // inner, center, outer
-  outerRadius: 300, // Manage size
-  pointerAngle: 0, // Test pointer angle
-  //   rotationAngle: -30,
-  segments: [
-    { fillStyle: "#eae56f", text: "Support" },
-    { fillStyle: "#89f26e", text: "Bottom" },
-    { fillStyle: "#7de6ef", text: "Middle" },
-    { fillStyle: "#e7706f", text: "Jungle" },
-    { fillStyle: "#7000FF", text: "Top" },
-  ],
-  // Test Winning segment arrow point angle
-  //   pointerGuide: {
-  //     display: true,
-  //     strokeStyle: "red",
-  //     lineWidth: 3,
-  //   },
+let theWheel; // Declare theWheel in the global scope
+// Define the API URL
+const apiUrl =
+  "http://ddragon.leagueoflegends.com/cdn/13.18.1/data/en_US/champion.json";
 
-  animation: {
-    type: "spinToStop", // Type of animation.
-    duration: 7, // How long the animation is to take in seconds.
-    spins: 4, // The number of complete 360 degree rotations the wheel is to do.
+// Use the Fetch API to make a GET request
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json(); // Parse the JSON response
+  })
+  .then((data) => {
+    // Handle the JSON data here
+    const championNames = Object.keys(data.data); // Extract champion names
+    // Create an array of wheel segments based on champion names
+    const wheelSegments = championNames.map((championName) => {
+      return {
+        fillStyle: "#CEA9FE", // Set the background color for the wheel segment
+        text: championName, // Assign the champion name as the text for the segment
+      };
+    });
 
-    // Remember to do something after the animation has finished specify callback function.
-    callbackFinished: "alertPrize()",
-  },
-});
+    // Makes the wheel
+    theWheel = new Winwheel({
+      canvasId: "canvas",
+      numSegments: championNames.length, // Sets that many segments that there is heroes!
+      //   fillStyle: "#5D00D3", // Sets background color for wheel
+      textFontSize: 11, // FontSize
+      lineWidth: 0.3, // How thick lines are in wheel
+      textAlignment: "outer", // inner, center, outer
+      outerRadius: 360, // Manage size
+      //   rotationAngle: -30, // Rotates spinwheel
+      segments: wheelSegments,
+
+      // Test Winning segment arrow point angle
+      //   pointerAngle: 0, // Test pointer angle
+      //   pointerGuide: {
+      //     display: true,
+      //     strokeStyle: "red",
+      //     lineWidth: 3,
+      //   },
+
+      animation: {
+        type: "spinToStop", // Type of animation.
+        duration: 7, // How long the animation is to take in seconds.
+        spins: 4, // The number of complete 360-degree rotations the wheel is to do.
+
+        // What happens after spin is finished. It calls alertRole function
+        callbackFinished: alertChampion,
+      },
+    });
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
 
 // This function called after the spin animation has stopped.
-function alertPrize() {
-  // Call getIndicatedSegment() function to return pointer to the segment pointed to on wheel.
+function alertChampion() {
+  // Call getIndicatedSegment() function to return the pointer to the segment pointed to on the wheel.
   let winningSegment = theWheel.getIndicatedSegment();
 
-  // Basic alert of the segment text which is the prize name.
-  alert("Your role in the next game is " + winningSegment.text + "!");
+  // Basic alert of the segment text which is the chosen champion.
+  alert("Your champion in the next game is " + winningSegment.text + "!");
 }
